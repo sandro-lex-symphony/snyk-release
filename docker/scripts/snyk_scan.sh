f4c29becba2ebff99033b39290aef558030636d0
Sandro Lex <sandro.lex@symphony.com>
2021-05-31 21:46:27 +0200

    add exclude

--- docker/scripts/snyk_scan.sh
+++ docker/scripts/snyk_scan.sh
@@ -2,9 +2,10 @@
 set -e
 
 openssl enc -d -aes-256-cbc -md sha512 -pbkdf2 -iter 100000 -salt -in ${HOME}/snyk-token -out ${HOME}/snyk-token.txt -k $SECRET 
-echo "snyk auth ..."                                                                                                                                                                                                                                                                      
-snyk auth $(cat ${HOME}/snyk-token.txt)                                                                                                                                                                                                                                                                              
-rm ${HOME}/snyk-token.txt 
+echo "snyk auth ..."
+TOKEN=$(cat ${HOME}/snyk-token.txt)                                                                                                                                                                                                                                                                
+snyk auth $TOKEN                                                                                                                                                                                                                                                                           
+
 
 cd $(basename $GIT_REPO .git)
 
@@ -13,14 +14,23 @@ if [ "$JAVA_PARAMS" != "filler" ]
 then
     PARAMS=" -- ${JAVA_PARAMS} "
 fi
+
+EXCLUDE=''
+if [ "$SNYK_EXCLUDE" != "filler" ]
+then
+    EXCLUDE=" --exclude ${SNYK_EXCLUDE} "
+fi
                                                                                                                                                                                                                                                                                           
 if [ "$SNYK_SCAN" = "monitor" ]                                                                                                                                                                                                                                                           
 then                                                                                                                                                                                                                                                                                      
     echo "Snyk Scan..."                                                                                                                                                                                                                                                                   
-    snyk monitor --all-projects --org=$SNYK_ORG $PARAMS || true                                                                                                                                                                                                     
-elif [ "$SNYK_SCAN" = "test" ]                                                                                                                                                                                                                                                            
+    snyk monitor --all-projects ${EXCLUDE} --org=$SNYK_ORG $PARAMS || true   
+    curl -H "Authorization: $TOKEN" "https://"                                                                                                                                                                                                 
+elif [ "$SNYK_SCAN" = "test" ]                                                                                                                                                                                                                                                  
 then                                                                                                                                                                                                                                                                                      
-    snyk test --all-projects --json-file-output=${HOME}/data/${PRJ}.json $PARAMS || true                                                                                                                                                                                                       
+    snyk test --all-projects ${EXCLUDE} --json-file-output=${HOME}/data/${PRJ}.json $PARAMS || true                                                                                                                                                                                                       
 else                                                                                                                                                                                                                                                                                      
     echo "Skiping Snyk Scan ..."                                                                                                                                                                                                                                                          
 fi
+
+rm ${HOME}/snyk-token.txt
