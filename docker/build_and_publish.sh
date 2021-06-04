1e223427ea2f609448db6434a68c01f71e9cba97
Sandro Lex <sandro.lex@symphony.com>
2021-06-04 10:37:10 +0200

    build docker file in orchestrator

--- docker/build_and_publish.sh
+++ docker/build_and_publish.sh
@@ -3,9 +3,10 @@ set -e
 
 #buildah bud -f docker/Dockerfile -t snyk-release docker/
 
-docker build -f docker/Dockefile -t snyk-release docker/
-docker tag snyk-release artifact.symphony.com/slex-reg-test/snyk-release:experimental
-docker push artifact.symphony.com/slex-reg-test/snyk-release:experimental
+#docker build -f docker/Dockefile -t snyk-release docker/
+DOCKER_BUILDKIT=1 docker build -f docker/Dockerfile --no-cache --secret id=master,src=master.txt --secret id=org_token_plain,src=token.txt  -t snyk-release docker/
+#docker tag snyk-release artifact.symphony.com/slex-reg-test/snyk-release:experimental
+#docker push artifact.symphony.com/slex-reg-test/snyk-release:experimental
 
 
 
