03e6671d324c12e851c7c32a03027d1ab81578e3
Sandro Lex <sandro.lex@symphony.com>
2021-09-23 21:53:42 +0200

    fixed typos

--- docker/scripts/report.sh
+++ docker/scripts/report.sh
@@ -5,10 +5,10 @@
 # descrypt snyk token
 openssl enc -d -aes-256-cbc -md sha512 -pbkdf2 -iter 100000 -salt -in ${HOME}/snyk-token -out ${HOME}/snyk-token.txt -k $SECRET 
 
-SNYK_TOKEN=$(cat ~/token.txt)
-echo "[DEFAULT]" > ~/.snykctl
-echo "group_token = $SNYK_TOKEN" >> ~/.snykctl
-echo "group_id = $SNYK_ORG"  >> ~/.snykctl
+SNYK_TOKEN=$(cat ~/snyk-token.txt)
+echo "[DEFAULT]" > ~/.snykctl.conf
+echo "group_token = $SNYK_TOKEN" >> ~/.snykctl.conf
+echo "group_id = $SNYK_ORG"  >> ~/.snykctl.conf
                                        
 # gather issues data from snyk server
 TABLE = $(snykctl issue-count $SNYK_ORG)
@@ -24,6 +24,6 @@ URL = "https://corporate.symphony.com/integration/v1/whi/simpleWebHookIntegratio
 curl -X POST -d "payload=$MESSAGE" $URL
 
 # cleanup
-rm ~/token.txt
+rm ~/snyk-token.txt
 rm ~/webhook-id.txt
-rm ~/.snykctl
+rm ~/.snykctl.conf
